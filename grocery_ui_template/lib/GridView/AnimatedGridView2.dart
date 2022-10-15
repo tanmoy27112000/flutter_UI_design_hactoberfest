abaaa6e13a051196f54e5d2799422914958e528c
Arnav2001 <arnavkul07@gmail.com>
2022-10-15 20:11:36 +0530

    New Animated Grid View

---
+++ grocery_ui_template/lib/GridView/AnimatedGridView2.dart
@@ -0,0 +1,59 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+
+class AnimatedGridView2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double _w = MediaQuery.of(context).size.width;
+    int columnCount = 3;
+
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Go Back"),
+          centerTitle: true,
+          brightness: Brightness.dark),
+      body: AnimationLimiter(
+        child: GridView.count(
+          physics:
+          BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+          padding: EdgeInsets.all(_w / 60),
+          crossAxisCount: columnCount,
+          children: List.generate(
+            50,
+                (int index) {
+              return AnimationConfiguration.staggeredGrid(
+                position: index,
+                duration: Duration(milliseconds: 500),
+                columnCount: columnCount,
+                child: ScaleAnimation(
+                  duration: Duration(milliseconds: 900),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  child: ScaleAnimation(
+                    child: Container(
+                      margin: EdgeInsets.only(
+                          bottom: _w / 30, left: _w / 60, right: _w / 60),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 40,
+                            spreadRadius: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
