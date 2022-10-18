9d770648ceea8f5d680a44ad29895769a4960820
Harshith1263 <101880547+Harshith1263@users.noreply.github.com>
2022-10-18 20:56:26 +0530

    Updated Home Page UI
    Sir Now it Would be Fine I Guess !!

--- Calculator_App/lib/main.dart
+++ Calculator_App/lib/main.dart
@@ -1,59 +1,79 @@
-import 'package:calculator/pages/Advanced.dart';
 import 'package:calculator/pages/Scientific.dart';
+import 'package:calculator/pages/Advanced.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MaterialApp(home: Firstpage()));
 
 class Firstpage extends StatelessWidget {
   const Firstpage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     //Pre Defining the Variable
     Color shadowColor = Colors.red;
-
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return MaterialApp(
       title: 'Calculator',
       home: Scaffold(
+
+          //Multiple Widgets In A Coloum
           body: Column(
-        //Multiple Widgets In A Coloum
         children: <Widget>[
-          Container(
-              padding: const EdgeInsets.fromLTRB(60, 100, 55, 12),
-              color: Colors.black,
-              child: Text('Calculator',
-                  style: TextStyle(
-                      fontSize: 60.0,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 2.0,
-                      color: Colors.white70,
-                      //For Blurring Effects
-                      shadows: [
-                        Shadow(
-                          color: shadowColor,
-                          blurRadius: 20,
-                        ),
-                        Shadow(
-                          color: shadowColor,
-                          blurRadius: 40,
-                        ),
-                      ]))),
+          //Just a Container For Text
+          Flexible(
+              child: Container(
+                  height: height / 3,
+                  width: width,
+                  padding: EdgeInsets.fromLTRB(
+                      width / 6, height / 8, (width / 6), height / 8),
+                  color: Colors.black,
+                  child: Center(
+                      child: Text('Calculator',
+                          style: TextStyle(
+                              fontSize: 55,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 2.0,
+                              color: Colors.white70,
+                              //For Blurring Effects
+                              shadows: [
+                                Shadow(
+                                  color: shadowColor,
+                                  blurRadius: 20,
+                                ),
+                                Shadow(
+                                  color: shadowColor,
+                                  blurRadius: 40,
+                                ),
+                              ]))))),
+
           //Inserting an Image
-          const Center(
-            child: Image(
-              image: AssetImage('android/Assets/IMG_20220609_163850.jpg'),
-            ),
-          ),
+          // ignore: sized_box_for_whitespace
+          Flexible(
+              child: Container(
+                  color: Colors.black,
+                  height: (height / 3),
+                  width: width,
+                  child: const Expanded(
+                    child: Center(
+                      child: Image(
+                        image: AssetImage(
+                            'android/Assets/IMG_20220609_163850.jpg'),
+                      ),
+                    ),
+                  ))),
 
           //Inserting Buttons
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
+          Flexible(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
                 //List of Widgets In A Row
 
                 Container(
-                  padding: const EdgeInsets.fromLTRB(63.1, 60, 0.1, 84.857),
+                  height: height / 3,
+                  width: width / 2,
                   color: Colors.black,
+                  padding: EdgeInsets.fromLTRB(15, height / 8, 15, height / 8),
                   child: TextButton(
                       onPressed: () {
                         //Navigation to Another Screen
@@ -65,14 +85,15 @@ class Firstpage extends StatelessWidget {
                         );
                       },
                       style: TextButton.styleFrom(
-                          side: const BorderSide(color: Colors.red, width: 1),
+                          side: const BorderSide(
+                              color: Colors.redAccent, width: 1),
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(10),
                           )),
                       child: Text(
-                        'Scientific',
+                        'Normal',
                         style: TextStyle(
-                            fontSize: 20.0,
+                            fontSize: 20.2,
                             fontWeight: FontWeight.bold,
                             letterSpacing: 2.0,
                             color: Colors.white70,
@@ -91,8 +112,10 @@ class Firstpage extends StatelessWidget {
 
                 //Inserting Another Button
                 Container(
+                    height: height / 3,
+                    width: width / 2,
                     padding:
-                        const EdgeInsets.fromLTRB(33, 60, 71.22471, 84.857),
+                        EdgeInsets.fromLTRB(15, height / 8, 15, height / 8),
                     color: Colors.black,
                     child: TextButton(
                         onPressed: () {
@@ -104,13 +127,14 @@ class Firstpage extends StatelessWidget {
                           );
                         },
                         style: TextButton.styleFrom(
-                            side: const BorderSide(color: Colors.red, width: 1),
+                            side: const BorderSide(
+                                color: Colors.redAccent, width: 1),
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10),
                             )),
                         child: (Text('Advanced',
                             style: TextStyle(
-                                fontSize: 20.0,
+                                fontSize: 21.0,
                                 fontWeight: FontWeight.bold,
                                 letterSpacing: 2.0,
                                 color: Colors.white70,
@@ -124,9 +148,10 @@ class Firstpage extends StatelessWidget {
                                     blurRadius: 40,
                                   ),
                                 ])))))
-              ])
+              ]))
         ],
       )),
     );
   }
 }
+
