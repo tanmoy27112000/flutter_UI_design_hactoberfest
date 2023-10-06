94fc2a147a68d19b097a780938e9fba6a0a30db1
Tosin2289 <ayoolatosin00@gmail.com>
2023-10-06 13:56:32 +0100

    hotel_app_ui

--- Hotel app/hotel_app/lib/widgets/hotel_packages.dart
+++ Hotel app/hotel_app/lib/widgets/hotel_packages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hotel_app/screens/detailspage.dart';
 
 import '../models/room.dart';
 
@@ -12,8 +13,8 @@ class HotelPackages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 200,
+    return Expanded(
+      flex: 2,
       child: ListView.builder(
         shrinkWrap: true,
         scrollDirection: Axis.vertical,
@@ -23,122 +24,136 @@ class HotelPackages extends StatelessWidget {
             padding: const EdgeInsets.symmetric(
               vertical: 10.0,
             ),
-            child: Container(
-              height: 120,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 10,
-                    spreadRadius: 3,
-                    color: Colors.grey.shade400,
-                    offset: const Offset(-1, 2),
-                  ),
-                ],
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                      child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(10),
-                            bottomLeft: Radius.circular(10)),
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage(rooms[index].image),
-                        )),
-                  )),
-                  Expanded(
-                      flex: 2,
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(10),
-                              bottomRight: Radius.circular(10)),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Stack(
-                            children: [
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(rooms[index].title),
-                                  const SizedBox(
-                                    height: 5,
-                                  ),
-                                  Text(
-                                    rooms[index].subtitle,
-                                    textAlign: TextAlign.center,
-                                    style: const TextStyle(color: Colors.grey),
-                                  ),
-                                  const SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    rooms[index].price,
-                                    style: const TextStyle(color: Colors.blue),
-                                  ),
-                                  const SizedBox(
-                                    height: 5,
-                                  ),
-                                  Center(
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: const [
-                                        Icon(Icons.car_repair,
-                                            color: Colors.blue, size: 20),
-                                        SizedBox(
-                                          width: 5,
-                                        ),
-                                        Icon(Icons.shopping_basket,
-                                            color: Colors.blue, size: 20),
-                                        SizedBox(
-                                          width: 5,
-                                        ),
-                                        Icon(Icons.wine_bar,
-                                            color: Colors.blue, size: 20),
-                                        SizedBox(
-                                          width: 5,
-                                        ),
-                                        Icon(Icons.wifi,
-                                            color: Colors.blue, size: 20),
-                                      ],
-                                    ),
-                                  )
-                                ],
-                              ),
-                              Align(
-                                alignment: Alignment.centerRight,
-                                child: Container(
-                                  height: 50,
-                                  width: 60,
-                                  decoration: const BoxDecoration(
-                                    color: Colors.blue,
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(10),
-                                        bottomLeft: Radius.circular(10)),
-                                  ),
-                                  child: const Align(
-                                      alignment: Alignment.centerRight,
-                                      child: Text(
-                                        "Book",
-                                        style: TextStyle(
-                                            color: Colors.white, fontSize: 18),
-                                      )),
-                                ),
-                              ),
-                            ],
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) {
+                    return DetailsPage(
+                      rooms: [rooms[index]],
+                    );
+                  },
+                ));
+              },
+              child: Container(
+                height: 120,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 10,
+                      spreadRadius: 3,
+                      color: Colors.grey.shade400,
+                      offset: const Offset(-1, 2),
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              bottomLeft: Radius.circular(10)),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage(rooms[index].image),
+                          )),
+                    )),
+                    Expanded(
+                        flex: 2,
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(10),
+                                bottomRight: Radius.circular(10)),
                           ),
-                        ),
-                      )),
-                ],
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Stack(
+                              children: [
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(rooms[index].title),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text(
+                                      rooms[index].subtitle,
+                                      textAlign: TextAlign.left,
+                                      style:
+                                          const TextStyle(color: Colors.grey),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      rooms[index].price,
+                                      style:
+                                          const TextStyle(color: Colors.blue),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Center(
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: const [
+                                          Icon(Icons.car_repair,
+                                              color: Colors.blue, size: 20),
+                                          SizedBox(
+                                            width: 5,
+                                          ),
+                                          Icon(Icons.shopping_basket,
+                                              color: Colors.blue, size: 20),
+                                          SizedBox(
+                                            width: 5,
+                                          ),
+                                          Icon(Icons.wine_bar,
+                                              color: Colors.blue, size: 20),
+                                          SizedBox(
+                                            width: 5,
+                                          ),
+                                          Icon(Icons.wifi,
+                                              color: Colors.blue, size: 20),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Container(
+                                    height: 50,
+                                    width: 60,
+                                    decoration: const BoxDecoration(
+                                      color: Colors.blue,
+                                      borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(10),
+                                          bottomLeft: Radius.circular(10)),
+                                    ),
+                                    child: const Align(
+                                        alignment: Alignment.centerRight,
+                                        child: Text(
+                                          "Book",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18),
+                                        )),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )),
+                  ],
+                ),
               ),
             ),
           );
--- Hotel app/hotel_app/lib/widgets/popular_hotel.dart
+++ Hotel app/hotel_app/lib/widgets/popular_hotel.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/room.dart';
+import '../screens/detailspage.dart';
 
 class PopularHotel extends StatelessWidget {
   const PopularHotel({
@@ -23,81 +24,93 @@ class PopularHotel extends StatelessWidget {
             padding: const EdgeInsets.symmetric(
               horizontal: 10.0,
             ),
-            child: Container(
-              height: 200,
-              width: 160,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: [
-                  Expanded(
-                      child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(10),
-                            topRight: Radius.circular(10)),
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage(rooms[index].image),
-                        )),
-                  )),
-                  Expanded(
-                      child: Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(10),
-                          bottomRight: Radius.circular(10)),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Center(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text(rooms[index].title),
-                            ),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                              rooms[index].subtitle,
-                              style: const TextStyle(color: Colors.grey),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  rooms[index].price,
-                                  style: const TextStyle(color: Colors.blue),
-                                ),
-                                Row(
-                                  children: const [
-                                    Text(
-                                      "4.5",
-                                      style: TextStyle(color: Colors.blue),
-                                    ),
-                                    Icon(
-                                      Icons.star,
-                                      size: 18,
-                                      color: Colors.blue,
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
-                          ],
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) {
+                    return DetailsPage(
+                      rooms: [rooms[index]],
+                    );
+                  },
+                ));
+              },
+              child: Container(
+                height: 200,
+                width: 160,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Expanded(
+                        child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10)),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage(rooms[index].image),
+                          )),
+                    )),
+                    Expanded(
+                        child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10)),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(rooms[index].title),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                rooms[index].subtitle,
+                                style: const TextStyle(color: Colors.grey),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    rooms[index].price,
+                                    style: const TextStyle(color: Colors.blue),
+                                  ),
+                                  Row(
+                                    children: const [
+                                      Text(
+                                        "4.5",
+                                        style: TextStyle(color: Colors.blue),
+                                      ),
+                                      Icon(
+                                        Icons.star,
+                                        size: 18,
+                                        color: Colors.blue,
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  )),
-                ],
+                    )),
+                  ],
+                ),
               ),
             ),
           );
